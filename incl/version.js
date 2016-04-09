a991ad2647426a6bff6eec9d45d28be0b0adc840
lonesentinel19 <racingfan14@frontier.com>
2016-04-09 08:11:57 -0400

    update and expand website, include releases folder
    The website is now expanded and perhaps more useful.

---
+++ incl/version.js
@@ -0,0 +1,17 @@
+currentVersion = 30; /* Must be updated often */
+function versionPrompt() {
+	var yourVers = localStorage.getItem("vers");
+	if ( yourVers == null || yourVers == "null" ) {
+		var vers = prompt("Please enter your BatchX version that you are using. Use only numbers. Enter in '0' if you do not have BatchX.");
+		localStorage.setItem("vers", vers);
+		} else {
+		if ( parseInt(yourVers, 10) < currentVersion ) {
+			document.getElementById("warning").innerHTML = "You are currently using an outdated version of BatchX (" + yourVers + "), and the most recent" +
+			" is " + currentVersion + ". Please <a href='batchx/update.html' onclick='updated();'>update</a>.";
+			}
+		}
+	}
+
+function updated() {
+	localStorage.setItem("vers", currentVersion);
+}
