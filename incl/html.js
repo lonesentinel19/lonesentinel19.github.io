f0baa4dee87e15d7c098c947afd231388a49e4c5
lonesentinel19 <racingfan14@frontier.com>
2016-06-23 09:22:16 -0400

    update website

---
+++ incl/html.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){document.getElementById('links').innerHTML = "<span class=\"large\">(</span> <a href=\"http://lonesentinel19.github.io/bx-debugger\">bx-debugger</a>" +
+				"<> <a href=\"http://lonesentinel19.github.io/batchx\">BatchX</a>" +
+				"<> <a href=\"http://lonesentinel19.github.io/gis2d\">gis2d</a><span class=\"large\">)</span>";
+				});
