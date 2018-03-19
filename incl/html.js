0e465c43adad458fc69f379592beac860dc474f9
lonesentinel19 <racingfan14@frontier.com>
2018-03-19 17:32:38 -0400

    remove old stuff

--- incl/html.js
+++ incl/html.js
@@ -1,3 +1,3 @@
-$(document).ready(function(){document.getElementById('links').innerHTML = "" + 
-				"<span class=\"large\">[</span> Welcome <span class=\"large\">]</span>";
-				});
+$(document).ready(function(){
+		document.getElementById('links').innerHTML = "" + "<span class=\"large\">[</span> Welcome <span class=\"large\">]</span>";
+});
